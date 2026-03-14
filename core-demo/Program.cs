3137bd0a2ea6c5b61dedf58f2eb85592ddf93838
unknown <fengandzhy@gmail.com>
2026-03-15 10:59:26 +1300

    code-demo

---
+++ core-demo/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace core_demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, C#");
+
+            string name = "Tom";
+            int age = 20;
+            bool isAdult = age >= 18;
+            PrintUserInfo(name,age,isAdult);
+        }
+        static void PrintUserInfo(string name,int age,bool isAdult)
+        {
+            Console.WriteLine($"My name is {name}.");
+            Console.WriteLine($"I am {age} years old.");
+            Console.WriteLine($"Adult:{isAdult}");
+        }
+
+    }
+}
---
+++ core-demo/basic/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace core_demo.basic
+{
+    internal class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public void Introduce()
+        {
+            Console.WriteLine($"My name is {Name}, and I am {Age} yesrs olds.");
+        }       
+    }
+}
